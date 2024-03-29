c5478e4de08a01f1523f692d6e30d3bf0e68b262
Romstar744 <romstar742@gmail.com>
2024-03-29 22:41:42 +0300

    5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.starostin.check_the_solution"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.starostin.check_the_solution"
