8302a9362cea9c710e3c02fa4e610dfa60042b68
Romstar744 <romstar742@gmail.com>
2024-03-29 22:36:39 +0300

    3

--- app/src/main/java/com/starostin/check_the_solution/MainActivity.kt
+++ app/src/main/java/com/starostin/check_the_solution/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+    private lateinit var binding: ActivityMainBinding
+    private var right = 0
+    private var lose = 0
+    private var all = 0
+    private var startTime: Long = 0L
+
 }
